e2f3bb64e00cc07e8f093b020bce328dbf4518bb
Anmol Pal <anmol111pal@gmail.com>
2025-04-05 18:21:52 +0530

    Initial commit

---
+++ test/sentimentanalysis.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Sentimentanalysis from '../lib/sentimentanalysis-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sentimentanalysis-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Sentimentanalysis.SentimentanalysisStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
